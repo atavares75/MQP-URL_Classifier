2e9b111016a6e7ab5b4b1bcc70f248a51c86dd7a
Alex Tavares <amtavares@wpi.edu>
2019-08-29 13:15:53 -0400

    Began on SVM algorithm

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -2,8 +2,13 @@
 
 
 def extractLexicalFeatures(urls):
-    feature_set = list()
+    features = list()
+    i = 0
     for url in urls:
-        pass
-
-    return feature_set
+        i = i + 1
+        if type(url) is str:
+            features.append([len(url), len(url.split('/')[0]), len(url.split('.')) - 1])
+        else:
+            print(i)
+            print(str(url))
+    return features
