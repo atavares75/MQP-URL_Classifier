17d3dbb20177177531abbefd6abd5e95afeeff41
Alex Tavares <alextavares@autoreg-214962.dyn.wpi.edu>
2019-09-03 15:46:05 -0400

    added more lexical features

--- RandomForestTest.py
+++ RandomForestTest.py
@@ -3,45 +3,21 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from FeatureExtraction import extractLexicalFeatures
 
 # Read in csv
-dataset = pd.read_csv('C:\\Users\\bdwan\\OneDrive\\Documents\\WPI\\.code\\data\\all_data_labeled.csv')
+dataset = pd.read_csv("data/all_data_labeled.csv")
 
 # Store URLs and their labels
 urls = dataset.iloc[:, 2].values
 labels = dataset.iloc[:, 1].values
 
-features = []
-lab = []
-
-i = 0
-
 # Extract some lexical features
-for url in urls:
-	i = i + 1
-	if type(url) is str:
-		features.append([len(url), len(url.split('/')[0]), len(url.split('.')) - 1])
-	else:
-		print(i)
-		print(str(url))
-
-#Covert benign URLs to 0 and malicious to 1		
-for label in labels:
-	if label == 'Normal':
-		lab.append(0)
-	else:
-		lab.append(1)
-
-#Add a missing feature, looking into problem - 288543 is missing a URL
-#features.append([13, 9, 6])
+features = extractLexicalFeatures(urls)
 
 #Convert to numpy arrays
 feature = np.asarray(features)
-ls = np.asarray(lab)
-
-#Reshape the arrays for SKLearn functions
-feature = np.reshape(feature, (-1,3))
-ls = np.reshape(ls, (-1,1))
+ls = np.asarray(labels)
 
 from sklearn.model_selection import train_test_split
 feature_train, feature_test, label_train, label_test = train_test_split(feature, ls, test_size=0.25, random_state=2)
--- SVM-linear.py
+++ SVM-linear.py
@@ -1,48 +1,44 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from FeatureExtraction import extractLexicalFeatures
-import sklearn.feature_extraction
-import sklearn.feature_selection
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.svm import LinearSVC
+import numpy as np
 
-url_data = pd.read_csv("data/all_data_labeled.csv")
+dataset = pd.read_csv("data/all_data_labeled.csv")
 
-# normal_data = url_data[url_data.label == 'Normal']
-# phish_data = url_data[url_data.label == 'phish']
-# malware_data = url_data[url_data.label == 'malware']
-# ransomware_data = url_data[url_data.label == 'ransomware']
-# botnet_data = url_data[url_data.label == 'BotnetC&C']
-#
-# print(normal_data)
-# print(phish_data)
-# print(malware_data)
-# print(ransomware_data)
-# print(botnet_data)
-print('CSV read')
-urls = url_data['url']
-xData = extractLexicalFeatures(urls)
-yData = url_data['label']
+# Store URLs and their labels
+urls = dataset.iloc[:, 2].values
+labels = dataset.iloc[:, 1].values
+features = extractLexicalFeatures(urls)
+
+#Convert to numpy arrays
+feature = np.asarray(features)
+ls = np.asarray(labels)
+
+# #Reshape the arrays for SKLearn functions
+# feature = np.reshape(feature, (-1,3))
+# ls = np.reshape(ls, (-1,1))
+
 print('Features Extracted')
 
-xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size = 0.25, random_state = 2)
+xTrain, xTest, yTrain, yTest = train_test_split(feature, ls, test_size = 0.25, random_state = 2)
 print('\nTraining data split\n')
-# print(xTrain)
-# print(yTrain)
-# print('-------------------------')
-# print(xTest)
-# print(yTest)
 
-svm = LinearSVC(max_iter=1500)
+
+svm = LinearSVC(dual=False, fit_intercept=False, max_iter=2000)
 print('\nModel Created\n')
-#example parameters
-#penalty=’l2’, loss=’squared_hinge’, dual=True, tol=0.0001, C=1.0, multi_class=’ovr’, fit_intercept=True, intercept_scaling=1, class_weight=None, verbose=0, random_state=None, max_iter=1000
 
 svm.fit(xTrain, yTrain)
 print('\nModel Fitted\n')
 
 prediction = svm.predict(xTest)
 print('\nPrediction finished\n')
+
+print('\nPrediction Data\n')
+print(prediction)
+print('\nTest Data\n')
+print(yTest)
 
 print(confusion_matrix(yTest, prediction))
 print(classification_report(yTest, prediction))
