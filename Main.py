a62e38f526a3fd49201dcc7801c8ff04f2708a29
Alex Tavares <amtavares@wpi.edu>
2019-09-17 10:54:54 -0400

    Auto stash before merge of "master" and "origin/master"
    Fixed merge conflicts

--- Main.py
+++ Main.py
@@ -5,18 +5,11 @@ import sys
 
 import pandas as pd
 
+from FeatureExtraction.FeatureExtraction import FeatureSet
 from ModelBuilder.AlgorithmFactory import AlgorithmFactory as af
 
 
-# from Metrics.	VisualizeResults import visualize, evaluateFeatures, featureVariability
-
-def output_all():
-    pass
-
-
-def output_wanted():
-    pass
-
+# from Metrics/VisualizeResults import visualize, evaluateFeatures, featureVariability
 
 def train_and_test(algorithm, train_feature_set, train_labels, test_feature_set, test_labels):
     print("Training")
@@ -27,9 +20,6 @@ def train_and_test(algorithm, train_feature_set, train_labels, test_feature_set,
     prediction = algorithm.predict(test_feature_set)
     print("DONE")
 
-# output_all()
-# ouptut_wanetd()
-
 
 def main(json_file):
     with open(json_file) as jf:
@@ -37,6 +27,16 @@ def main(json_file):
 
     algorithms = af.get_all_algorithms(run["algorithm"])
     train_data = pd.read_csv(run["training_set"])
+    test_data = pd.read_csv(run["testing_set"])
+    train_labels = train_data['label']
+    train_urls = train_data['url']
+    test_labels = test_data['label']
+    test_urls = test_data['url']
+    train_feature_set = np.asarray(FeatureSet(run["feature_set"], train_urls).df)
+    test_feature_set = np.asarray(FeatureSet(run["feature_set"], test_urls).df)
+
+    for algorithm in algorithms:
+        train_and_test(algorithm, train_feature_set, train_labels, test_feature_set, test_labels)
 
 
 main(sys.argv[1])
