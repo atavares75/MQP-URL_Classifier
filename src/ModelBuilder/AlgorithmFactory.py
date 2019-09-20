9389b11c175447e33e5f847057d26db4dca01779
Alex Tavares <amtavares@wpi.edu>
2019-09-20 12:47:03 -0400

    Put Algorithm class in model builder package

--- src/ModelBuilder/AlgorithmFactory.py
+++ src/ModelBuilder/AlgorithmFactory.py
@@ -2,7 +2,7 @@
 
 import json, sys
 
-from Algorithm import Algorithm
+from src.Algorithm import Algorithm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
