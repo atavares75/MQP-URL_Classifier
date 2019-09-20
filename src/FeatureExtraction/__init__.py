e6d2ea18f7639845f972be9a0e9327896e891e45
Alex Tavares <amtavares@wpi.edu>
2019-09-20 12:44:10 -0400

    Removed the config folder

--- src/FeatureExtraction/__init__.py
+++ src/FeatureExtraction/__init__.py
@@ -7,11 +7,11 @@ import requests
 
 zip_file_url = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
 
-if not os.path.exists('../data/top-1m.csv'):
+if not os.path.exists('../../data/top-1m.csv'):
     r = requests.get(zip_file_url)
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall('../data/')
-d = pandas.read_csv('../data/top-1m.csv', sep=',', header=None, names=['Domain'], index_col=0)
+    z.extractall('../../data/')
+d = pandas.read_csv('../../data/top-1m.csv', sep=',', header=None, names=['Domain'], index_col=0)
 alexaDict = d.to_dict('list')
 listOfDomains = list(alexaDict.values())
 alexaSet = set(listOfDomains[0])
