c8603fba34b7cc8c14f720c6e7b159c56b52fe19
Alex Tavares <amtavares@wpi.edu>
2019-09-22 21:05:34 -0400

    Main can now do batch runs

--- src/ModelBuilder/AlgorithmFactory.py
+++ src/ModelBuilder/AlgorithmFactory.py
@@ -2,7 +2,7 @@
 
 import json, sys
 
-from src.Algorithm import Algorithm
+from ModelBuilder.Algorithm import Algorithm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
